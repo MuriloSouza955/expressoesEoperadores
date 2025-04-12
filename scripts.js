f91fbd50e219f7f5a04b9c2b39c7ce918c4147ee
MuriloSouza955 <souza.murilo@gmail.com>
2025-04-12 16:17:14 -0300

    03-02 Operadores aritméticos

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// Operadores aritméticos
+
+console.log("Soma: ", 12 + 8)
+
+console.log("Concatenação: ", "12" + "8")
+
+console.log("Subtração: ", 12 - 8)
+
+console.log("Multiplicação: ", 12 * 8)
+
+console.log("Divisão: ", 12 / 8)
+
+console.log("Módulo ou Resto da divisão : ", 12 % 8)
+
+console.log("Exponenciação: ", 3 ** 2)
