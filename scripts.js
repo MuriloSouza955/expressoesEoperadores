205f21b4656d568c8dff4c97a99f4f64d349908f
MuriloSouza955 <souza.murilo@gmail.com>
2025-04-21 12:39:02 -0300

    03-05 Grouping operator

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 /*
-Ordem de Precedência
-Na programação, assim como na matemática, a ordem de precedência é fundamental para definir a sequência de operações a serem realizadas em uma expressão. A ordem de precedência determina qual operação será executada primeiro em expressões com múltiplos operadores. É importante conhecer a ordem de precedência para evitar erros de cálculo. Parênteses podem ser utilizados para alterar a ordem de execução das operações. Compreender a ordem de precedência é essencial para desenvolver aplicações precisas e evitar equívocos nos cálculos.
+Grouping Operator
+Nesta aula, foi abordado o uso do Group Operator, representado pelos parênteses, para definir a ordem de procedência das operações. Foi exemplificado como a utilização dos parênteses pode alterar o resultado de uma expressão matemática, priorizando determinadas operações. Foi mostrado um exemplo de cálculo de média, destacando a importância de definir a ordem das operações matemáticas. O Group Operator é uma ferramenta essencial para determinar a sequência correta de cálculos em JavaScript e outras linguagens de programação.
 */
 
 let x = 10;
