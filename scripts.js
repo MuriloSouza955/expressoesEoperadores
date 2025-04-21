73d259898ddd2be1fb1bcc6be68ab0a8c251d05c
MuriloSouza955 <souza.murilo@gmail.com>
2025-04-21 11:22:27 -0300

    Ordem de Precedência

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
-// Incrremento e decremento
-let numero = 10;
-numero++; // incremento
-console.log(numero);
-numero--; // decremento
-console.log(numero);
+/*
+Ordem de Precedência
+Na programação, assim como na matemática, a ordem de precedência é fundamental para definir a sequência de operações a serem realizadas em uma expressão. A ordem de precedência determina qual operação será executada primeiro em expressões com múltiplos operadores. É importante conhecer a ordem de precedência para evitar erros de cálculo. Parênteses podem ser utilizados para alterar a ordem de execução das operações. Compreender a ordem de precedência é essencial para desenvolver aplicações precisas e evitar equívocos nos cálculos.
+*/
+
+let x = 10;
+let y = 20;
+let z = 30;
+
+console.log(x + y * z);
+
+console.log((x + y) * z);
+
+console.log(x + (y * z));
+
+console.log(x + y * z);
