bae40a0087ed1ed3e66f483c58d5e67416b32fa2
MuriloSouza955 <souza.murilo@gmail.com>
2025-04-23 10:44:30 -0300

    03-07 Estritamente Igual e Diferente

--- scripts.js
+++ scripts.js
@@ -1,17 +1,19 @@
 /*
-Igual a e Diferente de
-Neste vídeo, exploramos os operadores lógicos "igual a" e "diferente de" em JavaScript. A diferença entre comparar conteúdo e tipo é destacada. O operador "igual a" verifica apenas o conteúdo, enquanto o operador "diferente de" compara o conteúdo. Exemplos práticos são fornecidos para ilustrar como esses operadores funcionam. É importante entender essa distinção ao realizar comparações em JavaScript.
+Estritamente Igual e Diferente
+Nesta aula, expliquei sobre os operadores de comparação estritamente igual a (===) e estritamente diferente de (!==) em JavaScript. O operador estritamente igual a compara tanto o valor quanto o tipo das variáveis, enquanto o estritamente diferente de faz o mesmo, mas retorna verdadeiro se os tipos forem diferentes. Recomendei o uso desses operadores para garantir comparações precisas, especialmente ao lidar com diferentes tipos de dados, evitando erros comuns, como concatenar em vez de somar valores.
+
 */
 
 let one = 1;
 let two = 2;
 
-console.log(one == two);
-console.log(one == 1);
-console.log(one == "1");
 
-console.log("==============="); 
+// Estritamente igual a
+console.log(one === 1);
+console.log(one === "1");
 
-console.log(one != two);
-console.log(one != 1);
-console.log(one != "1");
+// Estritamente diferente de
+console.log(one !== two);
+console.log(one !== 1);
+console.log(two !== 2);
+console.log(two !== "2");
